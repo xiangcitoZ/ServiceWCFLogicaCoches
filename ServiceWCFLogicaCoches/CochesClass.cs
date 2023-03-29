89f7eaaf4c2da831f5b9e3a1d1efbb7e23fdb115
xiangcitoZ <pzxiang1@gmail.com>
2023-03-29 14:21:47 +0200

    Add project files.

---
+++ ServiceWCFLogicaCoches/CochesClass.cs
@@ -0,0 +1,29 @@
+﻿using ServiceWCFLogicaCoches.Models;
+using ServiceWCFLogicaCoches.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceWCFLogicaCoches
+{
+    public class CochesClass : ICochesContract
+    {
+        private RepositoryCoches repo;
+
+        public CochesClass()
+        {
+            this.repo = new RepositoryCoches();
+        }
+        public Coche FindCoche(int idcoche)
+        {
+            return repo.FindCoche(idcoche);
+        }
+
+        public List<Coche> GetCoches()
+        {
+            return this.repo.GetCoches();
+        }
+    }
+}
---
+++ ServiceWCFLogicaCoches/Models/Coche.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceWCFLogicaCoches.Models
+{
+    [DataContract]
+    public class Coche
+    {
+        [DataMember]
+        public int IdCoche { get; set; }
+        [DataMember]
+        public string Marca { get; set; }
+        [DataMember]
+        public string Modelo { get; set; }
+        [DataMember]
+        public string Imagen { get; set; }
+
+    }
+}
